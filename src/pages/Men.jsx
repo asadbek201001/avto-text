5faad43dac4525719411b1121da7ece47efe7737
Ismingiz <email@example.com>
2025-12-11 21:05:35 +0500

    pusshing5

--- src/pages/Men.jsx
+++ src/pages/Men.jsx
@@ -4,13 +4,19 @@ import React from "react";
 
 //components
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+
+
 
 export default function Men() {
     return (
         <>
         <Header />
         
-        <h1>men</h1>
+        <h1>hello</h1>
+
+        <Footer />
         </>
     )
 }
